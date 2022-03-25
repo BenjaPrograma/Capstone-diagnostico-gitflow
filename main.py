a7231c4338d50efe5a860d453e9f26b664be0225
benjaprograma <bayon@uc.cl>
2022-03-25 02:27:28 -0300

    pregunta 4 done

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 import time
-
-
+from collections import defaultdict
+import re
 
 def read_dataset():
     all_data = []
@@ -13,9 +13,21 @@ def read_dataset():
     return all_data
 
 
+def top10_hashtags(all_data):
+    freq_dict = defaultdict(int)
+    for tweet in all_data:
+        text = tweet["content"]
+        hashtags = re.findall(r"#(\w+)", text)
+        for hash in hashtags:
+            freq_dict[hash] +=1
+    sorted_dict = dict(sorted(freq_dict.items(), key=lambda x: x[1], reverse=True))
+    return list(sorted_dict.keys())[:10]
+
 if __name__ == '__main__':
     a = time.time()
     all_data = read_dataset()
     print(len(all_data))
     b = time.time() - a
     print(b)
+
+    print(top10_hashtags(all_data))
